9f3d26eb5dd798559ac6b6ad580c6b17d6485e62
Vitaliy Tomin <highwaystar.ru@gmail.com>
2020-12-07 16:06:08 +0800

    tbx304: Add thermal-engine.conf from stock
    * exported by thermal-engine -o command  on
    Lenovo/LenovoTB-X304L/X304L:8.1.0/OPM1.171019.026/S001017_190709_ROW:user/release-keys

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
